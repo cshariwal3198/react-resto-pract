7dcacf300406b5be7c67f31df469802d9aea6d31
Chetan <cshariwal3198@gmail.com>
2024-02-16 15:56:09 +0530

    temp css changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Body } from './components/body-files/body-component';
 import { Header } from './components/header files/header-component';
 import { restoraunts } from './data/restoraunts';
+import './index.css';
 
 export const App = () => {
 
