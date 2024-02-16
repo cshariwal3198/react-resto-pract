2917cff0bb7175444cedd6ce9f31ea12e0e56100
Chetan <cshariwal3198@gmail.com>
2024-02-16 16:09:53 +0530

    temp change

--- src/components/header files/header-component.tsx
+++ src/components/header files/header-component.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
     const imageSource = 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/food-app-icon-food-explorer-design-template-8ae900e41ccbc0a2e1b48a85d239e389_screen.jpg?ts=1585237320';
 
     return (
-        <StyledWrapper>
+        <StyledWrapper className="shadow-xl px-10">
             <StyledImage src={imageSource} alt="Failed to load logo" />
             <ul className="flex items-center justify-between list-none text-xl font-serif">
                 <li>Home</li>
